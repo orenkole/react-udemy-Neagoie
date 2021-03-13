601c6064dd234bb7ba29659b6ae66aa03ac3d3e9
Oleh <orenkole@gmail.com>
2021-03-13 15:37:48 +0200

    82. Header Component. Adding Header component and links to home, shop page

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
+import HeaderComponent from './components/header-component/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
         <Route exact={true} path="/shop/" component={ShopPage} />
