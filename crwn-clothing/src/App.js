188d55e0446380109f4bff843139c0ca8fcadc2e
Oleh <orenkole@gmail.com>
2021-03-13 16:04:07 +0200

    85. Sign In Component. Create SignIn component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HeaderComponent from './components/header-component/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <HeaderComponent />
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
-        <Route exact={true} path="/shop/" component={ShopPage} />
+        <Route exact={true} path="/shop" component={ShopPage} />
+        <Route exact={true} path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
