1f30046e57c3980b03ec90c09267db23247a6265
Oleh <orenkole@gmail.com>
2021-03-15 21:55:17 +0200

    106. Sign In With Email and Password

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { signInWithGoogle } from "../../firebase/firebase.utis";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utis";
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
 import "./sign-in.styles.scss";
@@ -14,9 +14,15 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({email: "", password: ""})
+    const {email, password} = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: "", password: ""})
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   handleChange = (event) => {
